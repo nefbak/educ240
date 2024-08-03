c95c0fcafd034ebed1ae559dde367604c34c2a72
nab11 <nab11@sfu.ca>
2024-08-03 00:32:11 -0700

    first commit

---
+++ src/main/java/com/example/educ240/Educ240Application.java
@@ -0,0 +1,13 @@
+package com.example.educ240;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Educ240Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Educ240Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/educ240/Educ240ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.educ240;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Educ240ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
